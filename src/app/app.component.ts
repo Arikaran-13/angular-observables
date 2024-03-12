394f2e7c49501bb0af71f6110ae90da2aaffd04e
Arikaran-13 <arikaranofficial@gmail.com>
2024-03-12 20:37:27 +0530

    Added newTask and ShowTask component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,11 @@
+import { ElementRef } from '@angular/core';
+import { ViewChild } from '@angular/core';
 import { Component } from '@angular/core';
+import { map } from 'rxjs';
+import { fromEvent } from 'rxjs';
+import { filter } from 'rxjs';
+import { of } from 'rxjs';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +14,51 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-observables';
+  data: number[]=[];
+
+  @ViewChild("buttonEl")
+  buttonElem:ElementRef;
+
+  //Observable => .next() will emit the data or stream of data only if any subcriber() is present 
+  myObservable = new Observable((observer)=>{ // stream of data 
+    setTimeout(()=>{observer.next(1)},2000);
+    setTimeout(()=>{observer.next(2)},2000);
+    setTimeout(()=>{observer.next(3)},2000);
+    setTimeout(()=>{observer.next(4)},2000);
+    setTimeout(()=>{observer.next(5)},3000);
+    setTimeout(()=>{observer.next(8)},3000);
+    setTimeout(()=>{observer.next(6)},3000);
+  });
+
+  //Observer => subcriber for the emitted data
+  getDataFromObservable():void{ //Onclick
+    this.myObservable.subscribe(
+      (val:number)=>{   // next
+        this.data.push(val);
+      } ,
+      (err)=>{     // when some error thrown by Observable we can handle here
+        alert(err);
+      },
+      ()=>{   // After completetion of Data stream this logic will be executed
+        alert("Completed")
+      }
+    )
+  }
+
+  myObservable2 = of("hello","angular");
+  myObservable3 = of(1,2,3,4,5);
+
+  ngOnInit(){
+    let newOb = this.myObservable.pipe(
+       filter((val:number)=> val%2===0),
+       map((val)=> val+2)
+    ).subscribe((next)=>{console.log(next)}, (err)=>{console.log(err)} , ()=>{console.log("Completed")});
+  }
+  
+  
+  ngAfterViewChecked(){
+    console.log("From ng After view checked");
+    let myOb3 = fromEvent(this.buttonElem.nativeElement,'click');
+    myOb3.subscribe((event)=>{console.log(event)})
+  }
 }
---
+++ src/app/new-task/new-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: "new-task",
+    templateUrl: "./new-task.component.html",
+    styleUrls: ["./new-task.component.css"]
+})
+export class NewTaskComponent{
+
+}
